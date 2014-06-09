22a41d66d61383dfe6d5a588ea97ca00ba325105
Naeem Talukdar <naeem.talukdar@gmail.com>
2014-06-09 11:20:26 -0400

    Initializing node

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ exports.creds = {
   // Your mongo auth uri goes here
   // e.g. mongodb://username:server@mongoserver:10059/somecollection
   // nationalpark is the name of my mongo database
-  mongoose_auth_local: 'mongodb://heroku_app26187629:q6feqedq21086k7v873e4iapl5@ds053877.mongolab.com:53877/heroku_app26187629',
+  mongoose_auth_local: 'mongodb://localhost/mercedes',
   mongoose_auth_jitsu: 'mongodb://heroku_app26187629:q6feqedq21086k7v873e4iapl5@ds053877.mongolab.com:53877/heroku_app26187629'
 }
